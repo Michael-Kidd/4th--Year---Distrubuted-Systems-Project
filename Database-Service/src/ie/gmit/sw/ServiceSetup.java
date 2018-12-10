15d8daa87468f08793251a012d930ab54b838b1a
Michael Kidd <G00236920@gmit.ie>
2018-12-10 20:50:00 +0000

    Changed runner to match the project brief

--- Database-Service/src/ie/gmit/sw/ServiceSetup.java
+++ Database-Service/src/ie/gmit/sw/ServiceSetup.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class RMISetup {
+public class ServiceSetup {
 
 	public static void main(String[] args) throws Exception {
 		
